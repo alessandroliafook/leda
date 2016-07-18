5258cbf0a273bf8f6830433151432251e1f792e1
Alessandro Lia Fook Santos 115111170 <alessandrolfs@lcc2-32.lcc.ufcg.edu.br>
2016-07-18 18:15:59 -0300

    fazendo o linearSorting

--- R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
@@ -12,8 +12,41 @@ public class CountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array,int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+
+	Integer menor = array[0];
+	Integer maior = array[0];
+	
+	for(int i = leftIndex + 1; i <= rightIndex; i++){
+		
+		if(menor.compareTo(array[i]) > 0){
+			menor = array[i];
+		}
+		
+		if(maior.compareTo(array[i]) < 0){
+			maior = array[i];
+		}
+		
+	}
+
+	int length = (int) maior.intValue() - menor.intValue(); 
+	int[] arrayIndex = new int[length];
+	
+	for (int i = 0; i < array.length; i++) {
+
+		int index = array[i] - menor.intValue() - 1;
+		arrayIndex[index]++;
+				
+	}
+	
+	for (int i = 1; i < arrayIndex.length; i++) {
+		arrayIndex[i] += arrayIndex[i - 1];
+	}
+
+	for(int i = leftIndex; i <= rightIndex; i++){
+		
+	}
+	
+	
 	}
 
 }
