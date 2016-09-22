105a5e4d8d9a52ce92f4dcc0c6dd2e4389a24d55
alessandroliafook <alessandro.liafook@gmail.com>
2016-09-21 22:20:29 -0300

    roteiro 16 enviado

--- R16-01-Rot-ArvorePV-environment/src/main/java/adt/bt/Util.java
+++ R16-01-Rot-ArvorePV-environment/src/main/java/adt/bt/Util.java
@@ -4,25 +4,75 @@ import adt.bst.BSTNode;
 
 public class Util {
 
-
 	/**
-	 * A rotacao a esquerda em node deve subir e retornar seu filho a direita
+	 * A rotacao a esquerda em node deve subir o seu filho a direita e
+	 * retorna-lo em seguida
+	 *
 	 * @param node
-	 * @return
+	 * @return the right node or null if the right son of the node is empty or null.
 	 */
 	public static <T extends Comparable<T>> BSTNode<T> leftRotation(BSTNode<T> node) {
-		//TODO Implemente sua rotacao a esquerda aqui
-		throw new UnsupportedOperationException("Not implemented yet!");
+
+		if (node != null && !node.isEmpty()) {
+
+			BTNode<T> parent = node.getParent();
+			BTNode<T> right = node.getRight();
+
+			right.setParent(parent);
+			if (parent != null) {
+
+				if(parent.getRight().equals(node))
+					parent.setRight(right);
+
+				else
+					parent.setLeft(right);
+			}
+
+			node.setRight(right.getLeft());
+			if (node.getRight() != null)
+				node.getRight().setParent(node);
+
+			right.setLeft(node);
+			node.setParent(right);
+
+			return (BSTNode<T>) right;
+		}
+		return null;
 	}
 
 	/**
-	 * A rotacao a direita em node deve subir e retornar seu filho a esquerda
+	 * A rotacao a direita em node deve subir seu filho a esquerda s retorna-lo
+	 * em seguida
+	 *
 	 * @param node
 	 * @return
 	 */
 	public static <T extends Comparable<T>> BSTNode<T> rightRotation(BSTNode<T> node) {
-		//TODO Implemente sua rotacao a esquerda aqui
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
 
+		if (node != null && !node.isEmpty()) {
+
+			BTNode<T> parent = node.getParent();
+			BTNode<T> left = node.getLeft();
+
+			left.setParent(parent);
+			if (parent != null) {
+
+				if (parent.getLeft().equals(node))
+					parent.setLeft(left);
+
+				else
+					parent.setRight(left);
+			}
+
+			node.setLeft(left.getRight());
+			if (node.getLeft() != null)
+				node.getLeft().setParent(node);
+
+			left.setRight(node);
+			node.setParent(left);
+
+			return (BSTNode<T>) left;
+		}
+		return null;
+	}
 }
--- R16-01-Rot-ArvorePV-environment/src/main/java/adt/rbtree/RBTreeImpl.java
+++ R16-01-Rot-ArvorePV-environment/src/main/java/adt/rbtree/RBTreeImpl.java
@@ -1,104 +1,303 @@
 package adt.rbtree;
 
 import adt.bst.BSTImpl;
+import adt.bt.BTNode;
+import adt.bt.Util;
 import adt.rbtree.RBNode.Colour;
 
-public class RBTreeImpl<T extends Comparable<T>> extends BSTImpl<T>
-		implements RBTree<T> {
+import java.util.ArrayList;
+import java.util.List;
 
-	public RBTreeImpl() {
-		this.root = new RBNode<T>();
-	}
+public class RBTreeImpl<T extends Comparable<T>> extends BSTImpl<T> implements RBTree<T> {
 
-	protected int blackHeight() {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+   public RBTreeImpl() {
+      this.root = new RBNode<T>();
+   }
 
-	protected boolean verifyProperties() {
-		boolean resp = verifyNodesColour() && verifyNILNodeColour()
-				&& verifyRootColour() && verifyChildrenOfRedNodes()
-				&& verifyBlackHeight();
+   protected int blackHeight() {
+      if (isEmpty())
+         return 0;
 
-		return resp;
-	}
+      else {
 
-	/**
-	 * The colour of each node of a RB tree is black or red. This is guaranteed
-	 * by the type Colour.
-	 */
-	private boolean verifyNodesColour() {
-		return true; // already implemented
-	}
+         int leftHeight = blackHeightRecursive((RBNode<T>) root.getLeft());
+         int rightHeight = blackHeightRecursive((RBNode<T>) root.getRight());
 
-	/**
-	 * The colour of the root must be black.
-	 */
-	private boolean verifyRootColour() {
-		return ((RBNode<T>) root).getColour() == Colour.BLACK; // already
-																// implemented
-	}
+         return (leftHeight >= rightHeight) ? leftHeight : rightHeight;
+      }
+   }
 
-	/**
-	 * This is guaranteed by the constructor.
-	 */
-	private boolean verifyNILNodeColour() {
-		return true; // already implemented
-	}
+   public int blackHeightRecursive(RBNode<T> node) {
 
-	/**
-	 * Verifies the property for all RED nodes: the children of a red node must
-	 * be BLACK.
-	 */
-	private boolean verifyChildrenOfRedNodes() {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+      if (node.isEmpty())
+         return 1;
 
-	/**
-	 * Verifies the black-height property from the root. The method blackHeight
-	 * returns an exception if the black heights are different.
-	 */
-	private boolean verifyBlackHeight() {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+      else {
 
-	@Override
-	public void insert(T value) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+         int blackFactor = (node.getColour().equals(Colour.BLACK)) ? 1 : 0;
+         int leftHeight = blackHeightRecursive((RBNode<T>) node.getLeft()) + blackFactor;
+         int rightHeight = blackHeightRecursive((RBNode<T>) node.getRight()) + blackFactor;
 
-	@Override
-	public RBNode<T>[] rbPreOrder() {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+         return (leftHeight >= rightHeight) ? leftHeight : rightHeight;
+      }
+   }
 
-	// FIXUP methods
-	protected void fixUpCase1(RBNode<T> node) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+   protected boolean verifyProperties() {
+      boolean resp = verifyNodesColour() && verifyNILNodeColour() && verifyRootColour() && verifyChildrenOfRedNodes()
+            && verifyBlackHeight();
 
-	protected void fixUpCase2(RBNode<T> node) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+      return resp;
+   }
 
-	protected void fixUpCase3(RBNode<T> node) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+   /**
+    * The colour of each node of a RB tree is black or red. This is guaranteed
+    * by the type Colour.
+    */
+   private boolean verifyNodesColour() {
+      return true; // already implemented
+   }
 
-	protected void fixUpCase4(RBNode<T> node) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+   /**
+    * The colour of the root must be black.
+    */
+   private boolean verifyRootColour() {
+      return ((RBNode<T>) root).getColour() == Colour.BLACK; // already
+      // implemented
+   }
 
-	protected void fixUpCase5(RBNode<T> node) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+   /**
+    * This is guaranteed by the constructor.
+    */
+   private boolean verifyNILNodeColour() {
+      return true; // already implemented
+   }
+
+   /**
+    * Verifies the property for all RED nodes: the children of a red node must
+    * be BLACK.
+    */
+   private boolean verifyChildrenOfRedNodes() {
+
+      if (isEmpty())
+         return true;
+
+      else
+         return verifyChildrenOfRedNodesRecursive(castNode(getRoot()));
+   }
+
+   private boolean verifyChildrenOfRedNodesRecursive(RBNode<T> node) {
+
+      if (node.isEmpty()) {
+         return true;
+
+      } else if (node.getColour().equals(Colour.BLACK)
+            || (node.getColour().equals(Colour.RED) && castNode(node.getLeft()).getColour().equals(Colour.BLACK) && castNode(
+                  node.getRight()).getColour().equals(Colour.BLACK))) {
+
+         return verifyChildrenOfRedNodesRecursive(castNode(node.getLeft()))
+               && verifyChildrenOfRedNodesRecursive(castNode(node.getRight()));
+
+      } else
+         return false;
+   }
+
+   /**
+    * Verifies the black-height property from the root. The method blackHeight
+    * returns an exception if the black heights are different.
+    */
+   private boolean verifyBlackHeight() {
+      return verifyBlackHeightRecursive(castNode(getRoot()));
+   }
+
+   private boolean verifyBlackHeightRecursive(RBNode<T> node) {
+
+      if (node.isEmpty())
+         return true;
+
+      else if (blackHeightRecursive(castNode(node.getRight())) == blackHeightRecursive(castNode(node.getLeft()))) {
+
+         return verifyBlackHeightRecursive(castNode(node.getRight()))
+               && verifyBlackHeightRecursive(castNode(node.getLeft()));
+      }
+
+      else
+         throw new RuntimeException("The black heights are different.");
+   }
+
+   @Override
+   public void insert(T value) {
+
+      if (value == null)
+         return;
+
+      RBNode<T> node = insertRecursive(value, (RBNode<T>) getRoot());
+      fixUpCase1(node);
+   }
+
+   public RBNode<T> insertRecursive(T element, RBNode<T> node) {
+
+      if (node.isEmpty()) {
+
+         node.setData(element);
+         node.setColour(Colour.RED);
+
+         RBNode<T> newLeft = new RBNode<T>();
+         newLeft.setParent(node);
+         node.setLeft(newLeft);
+
+         RBNode<T> newRight = new RBNode<T>();
+         newRight.setParent(node);
+         node.setRight(newRight);
+
+         return node;
+
+      } else if (node.getData().compareTo(element) > 0) {
+         return insertRecursive(element, (RBNode<T>) node.getLeft());
+
+      } else if (node.getData().compareTo(element) < 0) {
+         return insertRecursive(element, (RBNode<T>) node.getRight());
+      }
+      return new RBNode<>();
+   }
+
+   @Override
+   public RBNode<T>[] rbPreOrder() {
+      int size = size();
+      List<RBNode<T>> list = new ArrayList<>();
+      RBNode<T>[] result = new RBNode[size];
+
+      if (!isEmpty())
+         insertPreOrder((RBNode<T>) root, list);
+
+      int index = 0;
+      for (RBNode<T> rbNode : list)
+         result[index++] = rbNode;
+
+      return result;
+   }
+
+   public void insertPreOrder(RBNode<T> node, List<RBNode<T>> arrayList) {
+
+      if (!node.isEmpty()) {
+
+         arrayList.add(node);
+         insertPreOrder((RBNode<T>) node.getLeft(), arrayList);
+         insertPreOrder((RBNode<T>) node.getRight(), arrayList);
+      }
+   }
+
+   // FIXUP methods
+   protected void fixUpCase1(RBNode<T> node) {
+      if (node == null)
+         return;
+
+      if (node.equals(getRoot()))
+         node.setColour(Colour.BLACK);
+
+      else
+         fixUpCase2(node);
+   }
+
+   protected void fixUpCase2(RBNode<T> node) {
+      if (verifyParent(node))
+         return;
+
+      if (!(((RBNode<T>) node.getParent()).getColour().equals(Colour.BLACK))) {
+         fixUpCase3(node);
+      }
+   }
+
+   protected void fixUpCase3(RBNode<T> node) {
+
+      RBNode<T> uncle = getUncle(node);
+      if (uncle.getColour().equals(Colour.RED)) {
+
+         RBNode<T> parent = (RBNode<T>) node.getParent();
+         RBNode<T> granParent = (RBNode<T>) parent.getParent();
+
+         parent.setColour(Colour.BLACK);
+         uncle.setColour(Colour.BLACK);
+         granParent.setColour(Colour.RED);
+         fixUpCase1(granParent);
+      }
+
+      else
+         fixUpCase4(node);
+   }
+
+   public RBNode<T> getUncle(RBNode<T> node) {
+
+      RBNode<T> parent = (RBNode<T>) node.getParent();
+      RBNode<T> granParent = (RBNode<T>) parent.getParent();
+      RBNode<T> uncle;
+      if (((RBNode<T>) granParent.getLeft()).equals(parent))
+         uncle = (RBNode<T>) granParent.getRight();
+      else
+         uncle = (RBNode<T>) granParent.getLeft();
+
+      return uncle;
+   }
+
+   protected void fixUpCase4(RBNode<T> node) {
+      RBNode<T> next = node;
+      RBNode<T> parent = (RBNode<T>) node.getParent();
+      RBNode<T> granParent = (RBNode<T>) parent.getParent();
+
+      if (((RBNode<T>) parent.getRight()).equals(node) && ((RBNode<T>) granParent.getLeft()).equals(parent)) {
+         RBNode<T> newRoot = (RBNode<T>) Util.leftRotation(parent);
+         if (newRoot.getParent() == null)
+            setRoot(newRoot);
+
+         next = (RBNode<T>) node.getLeft();
+      }
+
+      else if (((RBNode<T>) parent.getLeft()).equals(node) && ((RBNode<T>) granParent.getRight()).equals(parent)) {
+
+         RBNode<T> newRoot = (RBNode<T>) Util.rightRotation(parent);
+
+         if (newRoot.getParent() == null)
+            setRoot(newRoot);
+
+         next = (RBNode<T>) node.getRight();
+      }
+
+      fixUpCase5(next);
+   }
+
+   protected void fixUpCase5(RBNode<T> node) {
+      if (node == null)
+         return;
+
+      RBNode<T> parent = (RBNode<T>) node.getParent();
+      parent.setColour(Colour.BLACK);
+
+      RBNode<T> granParent = (RBNode<T>) parent.getParent();
+      granParent.setColour(Colour.RED);
+
+      if (((RBNode<T>) parent.getLeft()).equals(node)) {
+         RBNode<T> newRoot = (RBNode<T>) Util.rightRotation(granParent);
+
+         if (newRoot.getParent() == null)
+            setRoot(newRoot);
+      } else {
+         RBNode<T> newRoot = (RBNode<T>) Util.leftRotation(granParent);
+
+         if (newRoot.getParent() == null)
+            setRoot(newRoot);
+      }
+   }
+
+   protected RBNode<T> castNode(BTNode node) {
+      return (RBNode<T>) node;
+   }
+
+   protected  boolean verifyParent(RBNode<T> node) {
+      if (node.getParent() == null)
+         return true;
+
+      else
+         return false;
+
+   }
+
 }
